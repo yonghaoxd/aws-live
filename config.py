41a7849d0760118396b50aa48e7376f85715b8f6
yonghaoxd <113483321+yonghaoxd@users.noreply.github.com>
2023-08-29 11:41:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cwnqgqhaqqgj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "mahyonghao-employee"
+customregion = "us-east-1"
+
